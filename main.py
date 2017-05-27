54c68cfed63fb1a5070eaa4cc76cc8d399102d34
v582singh <vj582ijay@gmail.com>
2017-05-27 15:19:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ import database
 
 
 # Twilio Account Details
-ACCOUNT_SID = "AC5149d33e538a72c194a70f1b702db479"
-AUTH_TOKEN = "0385315cb14c37f2cc34350a24e1b979"
 
 client = TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN)
 #------------------------------------------------------------------------
@@ -17,8 +15,6 @@ def send_cmd(Event):
     for x in range(1,10):
         message = client.messages.create(
             body = database.retrieve(x),
-            to="+18053729380",
-            from_="+18054448126",
             )
     print (message.sid)
     #cursor = database.main()
